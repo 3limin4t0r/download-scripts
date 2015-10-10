416b652b9e990c092e7c56717e388618719a684e
Johan <djo.went@hotmail.com>
2015-10-10 16:45:09 +0200

    Create save_diablo3_wallpapers_to.rb
    script for downloading all Diablo 3 wallpapers

---
+++ save_diablo3_wallpapers_to.rb
@@ -0,0 +1,48 @@
+require 'net/http'
+
+# save wallpapers from http://eu.battle.net/d3/en/media/wallpapers/
+diablo_folder = ARGV[0]
+if diablo_folder.nil?
+  puts 'provide an location as argument'
+  exit
+end
+
+Dir.chdir(diablo_folder)
+
+uri = URI('http://eu.battle.net/d3/en/media/wallpapers/')
+resp = Net::HTTP.get_response(uri)
+unless resp.is_a? Net::HTTPSuccess
+  puts "error in response: #{resp}"
+  exit
+end
+
+folder_diablo = 'Diablo III wallpapers'
+Dir.mkdir(folder_diablo) unless Dir.exist? folder_diablo
+Dir.chdir(folder_diablo)
+
+regex = /var indices = \[((?:"wallpaper\d{3}",? ?)+)\];/
+images = resp.body.match(regex)[1].split(', ').map { |img| img.gsub(/"/, '') }
+
+images.each do |img|
+
+  params = { view: img }
+  uri.query = URI.encode_www_form(params)
+  resp = Net::HTTP.get_response(uri)
+  next unless resp.is_a? Net::HTTPSuccess
+
+  folder_xxx = img.match(/\d+/)[0]
+  Dir.mkdir(folder_xxx) unless Dir.exist? folder_xxx
+  Dir.chdir(folder_xxx)
+
+  regex = /class="format".*?href="([^-]+-(\d+x\d+-?\w*)\.(\w+))"/
+
+  resp.body.scan(regex) do |link, format, extension|
+    img_resp = Net::HTTP.get_response(URI(link))
+    next unless resp.is_a? Net::HTTPSuccess
+    open("#{format}.#{extension}", 'wb') { |file| file.write(img_resp.body) }
+  end
+
+  Dir.chdir('..')
+
+end
+
